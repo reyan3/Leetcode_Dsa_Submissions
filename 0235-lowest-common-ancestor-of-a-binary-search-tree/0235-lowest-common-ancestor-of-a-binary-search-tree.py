6ede11cbfb51e4a3bd7cad1dfb9392bd0c85c6bd
Reyan <clan6229@gmail.com>
2026-08-28 21:01:03 +0530

    Time: 79 ms (5.91%), Space: 22.6 MB (68.04%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -10,14 +10,11 @@ class Solution:
         
         while True:
             
-            # as in bst all left value is lower than root value
             if p.val < root.val and q.val < root.val:
                 root = root.left
             
-            # as in bst all right value is greater than root value
             elif p.val > root.val and q.val > root.val:
                 root = root.right
             
-            # if not greater or smaller value found then the intersection is always the root therefore return root
             else:
                 return root
