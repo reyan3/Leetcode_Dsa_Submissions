fd7de88134d9fd9003fea69e64bcc53a2b4d912e
Reyan <clan6229@gmail.com>
2026-09-26 20:41:57 +0530

    Time: 3 ms (78.11%), Space: 21.2 MB (56.89%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -8,22 +8,22 @@ class Solution:
     def buildTree(self, preorder: list[int], inorder: list[int]) -> TreeNode | None:
         d = {}
 
-        for i in range(len(inorder)):
-            d[inorder[i]] = i
+        for i,n in enumerate(inorder):
+            d[n] = i
         
-        def build(preStart , preEnd , inStart , inEnd):
-            if preStart > preEnd or inStart > inEnd:
+        def build(preSt , preEnd , inSt , inEnd):
+            if preSt > preEnd or inSt > inEnd:
                 return None
             
-            rootval = preorder[preStart]
+            rootval = preorder[preSt]
             root = TreeNode(rootval)
 
             inRoot = d[rootval]
-            numsLeft = inRoot - inStart
+            numsleft = inRoot - inSt
 
-            root.left = build(preStart + 1,preStart + numsLeft, inStart , inRoot-1)
-            root.right = build(preStart + numsLeft + 1 , preEnd , inRoot + 1 , inEnd)
+            root.left = build(preSt + 1 , preSt+numsleft , inSt , inRoot-1)
+            root.right = build(preSt + numsleft + 1 , preEnd , inRoot+1 , inEnd)
 
             return root
         
-        return build(0,len(preorder)-1 , 0, len(inorder)-1)
+        return build(0, len(preorder)-1, 0 , len(inorder) - 1)
